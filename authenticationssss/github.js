b33eee84b1b6d5df4430cc863a07203e63cc2a4a
Kayode Raimi <kayode.raimi123@gmail.com>
2024-03-08 12:23:07 +0100

    changed the callback link

--- authenticationssss/github.js
+++ authenticationssss/github.js
@@ -14,7 +14,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/github/callback"
+    callbackURL: "https://authentication-and-authorization-k6pd.onrender.com/auth/github/callback"
   },
    async function(accessToken, refreshToken, profile, done) {
     console.log(profile)
--- authenticationssss/google.js
+++ authenticationssss/google.js
@@ -17,7 +17,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/google/callback",
+    callbackURL: "https://authentication-and-authorization-k6pd.onrender.com/auth/google/callback",
     passReqToCallback   : true
   },
    async function(request, accessToken, refreshToken, profile, done) {
